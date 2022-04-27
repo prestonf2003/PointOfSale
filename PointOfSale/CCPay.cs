afb366f3aa3504b329916c7049fd9a2cbdafe9af
sadams31 <sadams@wwiflint.com>
2022-04-27 17:05:29 -0400

    Creation of Payment Methods and Pay Classes

---
+++ PointOfSale/CCPay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale
+{
+     class CCPay : PaymentMethod
+    {
+        public int CCNumber { get; set; }
+        public DateTime Expiration { get; set; }
+        public int CVV { get; set; } 
+        
+        public override void GetTotal()
+        {
+            base.GetTotal();
+
+            if (HowtoPay == "credit card");
+            {
+                Console.WriteLine("Please enter your credit card number.");
+                CCNumber = int.Parse(Console.ReadLine()); //Need to validate this is 16 digits long
+
+                Console.WriteLine("And what is the expiration date?");
+                Expiration = DateTime.Parse(Console.ReadLine()); // Need to validate this is a future date?
+
+                Console.WriteLine("And what is the CVV number?");
+                CVV = int.Parse(Console.ReadLine()); //Need to validate that this is 3 digits.
+            }
+        }
+
+    }
+}
---
+++ PointOfSale/CashPay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSale
+{
+     class CashPay : PaymentMethod
+    {
+        public double Cash { get; set; }
+        public double Change { get; set; }
+
+        public override void GetTotal()
+        {
+            base.GetTotal();
+            if(HowtoPay == "cash")
+            {
+                Console.WriteLine($"Your total was {GetTotal}. How much cash will you be paying with?");
+                Cash = double.Parse(Console.ReadLine());
+                if(Cash > GrandTotal)
+                {
+                    Change = Cash - Grandtotal;
+                    Console.WriteLine($"Your change is {Change}");
+                }
+            }
+        }
+    }
+}
