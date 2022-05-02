c42aba527f6785c41a53584efc8879ed886b6ef2
Daniel Sancricca <dansancricca@gmail.com>
2022-05-02 14:10:36 -0400

    added try catch for gibberish case in quantity and item select

--- PointOfSale/Program.cs
+++ PointOfSale/Program.cs
@@ -221,22 +221,28 @@ namespace PointOfSale
                 Console.WriteLine((i + 1) + " " + p.name + " " + "$" + p.price);
             }
             Console.WriteLine();
-            int ChooseItem = int.Parse(GetUserInput("Which Item would you like"));
-            if(ChooseItem < 0 || ChooseItem > productList.Count)
+            try
             {
-                Console.WriteLine("That was not a valid number please try again");
+                int ChooseItem = int.Parse(GetUserInput("Which Item would you like"));
+                if (ChooseItem < 0 || ChooseItem > productList.Count)
+                {
+                    Console.WriteLine("That was not a valid number please try again");
+                    goto start;
+                }
+                Product Chosen = productList[ChooseItem - 1];
+
+                int QuantityWanted = int.Parse(GetUserInput("How Many"));
+                double price = Chosen.price * QuantityWanted;
+                subtotal.Add(price);
+                Quantities.Add(QuantityWanted);
+                Cart.Add(Chosen.name);
+                Sale.Add(price);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("I'm sorry that is not a number we can use. Please try again.");
                 goto start;
             }
-                Product Chosen = productList[ChooseItem - 1];
-            
-                
-              
-            int QuantityWanted = int.Parse(GetUserInput("How Many"));
-            double price = Chosen.price * QuantityWanted;
-            subtotal.Add(price);
-            Quantities.Add(QuantityWanted);
-            Cart.Add(Chosen.name);
-            Sale.Add(price);
 
         }
         static void PrintCart()
